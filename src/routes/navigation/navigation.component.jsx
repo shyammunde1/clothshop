1598b3d16283d1ac140e720f1450ba8a79351e96
shyammunde1 <shyammunde1@gmail.com>
2023-08-14 16:33:21 +0530

    navigationbar craeted and deployed

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import logo from '../../assets/cloth-icon.png'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link to='/'  className="logo-container">
+          <img src={logo} alt='cloth-icon-logo' />
+        </Link>
+        <div className="nav-links-container">
+          <Link to={"/shop"} className="nav-link">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
