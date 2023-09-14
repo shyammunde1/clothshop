a66f8558cef4e0171b68db1ee4cdc303feadc370
samtech <shyammunde1@gmail.com>
2023-09-14 09:58:32 +0530

    deploy the manifest

--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -1,43 +1,44 @@
-import DirectoryItem from '../directory-item/directory-item.component';
+import DirectoryItem from "../directory-item/directory-item.component";
 
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 const categories = [
   {
     id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    route: 'shop/hats'
+    title: "hats",
+    imageUrl:
+      "https://images.unsplash.com/photo-1674574124349-0928f4b2bce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+    route: "shop/hats",
   },
   {
     id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    route: 'shop/jackets'
+    title: "jackets",
+    imageUrl: "https://images.unsplash.com/photo-1487744480471-9ca1bca6fb7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1491&q=80",
+    route: "shop/jackets",
   },
   {
     id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    route: 'shop/sneakers'
+    title: "sneakers",
+    imageUrl: "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1450&q=80",
+    route: "shop/sneakers",
   },
   {
     id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    route: 'shop/womens'
+    title: "womens",
+    imageUrl: "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    route: "shop/womens",
   },
   {
     id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    route: 'shop/mens'
+    title: "mens",
+    imageUrl: "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fG1lbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    route: "shop/mens",
   },
 ];
 
 const Directory = () => {
   return (
-    <div className='directory-container'>
+    <div className="directory-container">
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category} />
       ))}
